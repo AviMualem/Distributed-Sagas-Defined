f151f3228ec73d570330d4ad326dd2e9f95f2f7f
avii-wix <avii@wix.com>
2019-01-23 23:45:49 +0200

    RTFM

--- pre-test.sh
+++ pre-test.sh
@@ -6,10 +6,6 @@ MONGO_PORT1=37017
 MONGO_PORT2=37018
 MONGO_PORT3=37019
 
-echo 127.0.0.1 localmongo1 >> /etc/hosts
-echo 127.0.0.1 localmongo2 >> /etc/hosts
-echo 127.0.0.1 localmongo3 >> /etc/hosts
-
 CONNECTION_STRING="mongodb://localhost:${MONGO_PORT1},localhost:${MONGO_PORT2},localhost:${MONGO_PORT3}/${DB_NAME}?replicaSet=rs0"
 
 declare -a arr=("apartment-service" "book-trip-sec" "car-service" "restaurant-service")
